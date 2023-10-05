caeb7bef1aa6bbcf88dc7f16aef8445a14497a14
rafaelwdc <rafaelmfreitas93@gmail.com>
2023-10-05 20:50:49 -0300

    atualizando imagem no header

--- src/lib/AlurakutCommuns.js
+++ src/lib/AlurakutCommuns.js
@@ -24,7 +24,7 @@ export function AlurakutMenu({ githubUser }) {
   return (
     <AlurakutMenu.Wrapper isMenuOpen={isMenuOpen}>
       <div className="container">
-        <AlurakutMenu.Logo src={`${BASE_URL}/logo.svg`} />
+        <AlurakutMenu.Logo src={`https://www.orkut.com/img/orkut-logo.png`} />
 
         <nav style={{ flex: 1 }}>
           {[{ name: 'Inicio', slug: '/'}, {name: 'Amigos', slug: '/amigos'}, {name: 'Comunidades', slug: '/comunidades'}].map((menuItem) => (
